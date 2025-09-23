caf24369edad0481dd78b37eb627f95ad0607932
Milagros Razuc <126362614+milagros-razuc@users.noreply.github.com>
2025-09-22 23:01:17 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 
 const app = require('./src/app');
 
-// AHORA SÍ puedes usar process.env.PORT
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
-  console.log(`🚀 Servidor Cuerpo Sano API corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor Cuerpo Sano corriendo en http://localhost:${PORT}`);
 });
 
 process.on('SIGINT', () => {
